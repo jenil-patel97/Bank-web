8f2d2a0607b9c9e2a9c118c6cee8d6904e730ade
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-08-16 14:09:40 +0530

    feat: State component

--- my-bank-app/src/App.jsx
+++ my-bank-app/src/App.jsx
@@ -30,8 +30,8 @@ const App = () => {
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          {/* <Stats />
-          <Business />
+          <Stats />
+          {/* <Business />
           <Billing />
           <CardDeal />
           <Testimonials />
--- my-bank-app/src/constants/index.js
+++ my-bank-app/src/constants/index.js
@@ -71,23 +71,23 @@ export const navLinks = [
 //   },
 // ];
 
-// export const stats = [
-//   {
-//     id: "stats-1",
-//     title: "User Active",
-//     value: "3800+",
-//   },
-//   {
-//     id: "stats-2",
-//     title: "Trusted by Company",
-//     value: "230+",
-//   },
-//   {
-//     id: "stats-3",
-//     title: "Transaction",
-//     value: "$230M+",
-//   },
-// ];
+export const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$230M+",
+  },
+];
 
 // export const footerLinks = [
 //   {
