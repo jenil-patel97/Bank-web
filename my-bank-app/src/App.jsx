a34254bb180169050d3cf73f0732d9e530fdc7db
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-08-18 17:26:47 +0530

    feat: Billing component

--- my-bank-app/src/App.jsx
+++ my-bank-app/src/App.jsx
@@ -32,8 +32,8 @@ const App = () => {
         <div className={`${styles.boxWidth}`}>
           <Stats />
           <Business />
-          {/* <Billing />
-          <CardDeal />
+          <Billing />
+          {/* <CardDeal />
           <Testimonials />
           <Clients />
           <CTA />
--- my-bank-app/src/components/Billing.jsx
+++ my-bank-app/src/components/Billing.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import apple from "../assets/apple.svg";
+import bill from '../assets/bill.png'
+import google from '../assets/google.svg'
+import styles from "../style";
+import { layout } from '../style'
 
-const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+const Billing = () => (
+  <section id="product" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
-export default Billing
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+    </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Easily control your <br className="sm:block hidden" /> billing &
+        invoicing
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds
+        of credit cards on the market.
+      </p>
+
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img src={apple} alt="apple_store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+        <img src={google} alt="google_play" className="w-[144px] h-[43px] object-contain cursor-pointer" />
+      </div>
+    </div>
+  </section>
+);
+
+export default Billing;
